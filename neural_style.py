82b7209d24ff0cc93b271cffe0bcbd8659ff6421
Shafeen Tejani <shafeen.tejani@gmail.com>
2016-12-27 13:36:10 +0000

    Optimize input image to contain the style of one image and content of another

---
+++ neural_style.py
@@ -0,0 +1,129 @@
+import os
+
+import numpy as np
+import scipy.misc
+from os.path import exists
+from sys import stdout
+
+from style_transfer import StyleTransfer
+
+import math
+from argparse import ArgumentParser
+
+# default arguments
+CONTENT_WEIGHT = 5e1
+STYLE_WEIGHT = 1e2
+TV_WEIGHT = 1e2
+LEARNING_RATE = 1e1
+ITERATIONS = 1000
+VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--content',
+            dest='content', help='content image',
+            metavar='CONTENT', required=True)
+    parser.add_argument('--style',
+            dest='style', help='one or more style images',
+            metavar='STYLE', required=True)
+    parser.add_argument('--output',
+            dest='output', help='output path',
+            metavar='OUTPUT', required=True)
+    parser.add_argument('--iterations', type=int,
+            dest='iterations', help='iterations (default %(default)s)',
+            metavar='ITERATIONS', default=ITERATIONS)
+    parser.add_argument('--print-iterations', type=int,
+            dest='print_iterations', help='statistics printing frequency',
+            metavar='PRINT_ITERATIONS')
+    parser.add_argument('--checkpoint-output',
+            dest='checkpoint_output', help='checkpoint output format, e.g. output%%s.jpg',
+            metavar='OUTPUT')
+    parser.add_argument('--checkpoint-iterations', type=int,
+            dest='checkpoint_iterations', help='checkpoint frequency',
+            metavar='CHECKPOINT_ITERATIONS')
+    parser.add_argument('--content-weight', type=float,
+            dest='content_weight', help='content weight (default %(default)s)',
+            metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
+    parser.add_argument('--style-weight', type=float,
+            dest='style_weight', help='style weight (default %(default)s)',
+            metavar='STYLE_WEIGHT', default=STYLE_WEIGHT)
+    parser.add_argument('--tv-weight', type=float,
+            dest='tv_weight', help='total variation regularization weight (default %(default)s)',
+            metavar='TV_WEIGHT', default=TV_WEIGHT)
+    parser.add_argument('--learning-rate', type=float,
+            dest='learning_rate', help='learning rate (default %(default)s)',
+            metavar='LEARNING_RATE', default=LEARNING_RATE)
+    parser.add_argument('--initial',
+            dest='initial', help='initial image',
+            metavar='INITIAL')
+    return parser
+
+
+def load_image(image_path):
+    assert exists(image_path), "image {} does not exist".format(image_path)
+    img = imread(image_path)
+    img = img.astype("float32")
+    img = np.ndarray.reshape(img, (1,) + img.shape)
+    return img
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+
+    if not os.path.isfile(VGG_PATH):
+        parser.error("Network %s does not exist." % VGG_PATH)
+
+    content_image = load_image(options.content)
+    style_image = load_image(options.style)
+
+    initial = options.initial
+    if initial is not None:
+        initial = scipy.misc.imresize(imread(initial), content_image.shape[:2])
+
+    if options.checkpoint_output and "%s" not in options.checkpoint_output:
+        parser.error("To save intermediate images, the checkpoint output "
+                     "parameter must contain `%s` (e.g. `foo%s.jpg`)")
+
+    style_transfer = StyleTransfer(
+        vgg_path=VGG_PATH,
+        content=content_image,
+        style=style_image,
+        content_weight=options.content_weight,
+        style_weight=options.style_weight,
+        tv_weight=options.style_weight,
+        initial=initial)
+
+    for iteration, image, losses in style_transfer.train(
+        learning_rate=options.learning_rate,
+        iterations=options.iterations,
+        checkpoint_iterations=options.checkpoint_iterations
+    ):
+        print_losses(losses)
+
+        output_file = None
+        if iteration is not None:
+            if options.checkpoint_output:
+                output_file = options.checkpoint_output % iteration
+        else:
+            output_file = options.output
+        if output_file:
+            imsave(output_file, image)
+
+
+def print_losses(losses):
+    stdout.write('  content loss: %g\n' % losses['content'])
+    stdout.write('    style loss: %g\n' % losses['style'])
+    stdout.write('       tv loss: %g\n' % losses['total_variation'])
+    stdout.write('    total loss: %g\n' % losses['total'])
+
+def imread(path):
+    return scipy.misc.imread(path).astype(np.float)
+
+
+def imsave(path, img):
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    scipy.misc.imsave(path, img)
+
+
+if __name__ == '__main__':
+    main()
---
+++ style_transfer.py
@@ -0,0 +1,162 @@
+import vgg_network
+
+import tensorflow as tf
+import numpy as np
+
+from sys import stdout
+from functools import reduce
+
+
+class StyleTransfer:
+    CONTENT_LAYER = 'relu4_2'
+    STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+
+    def __init__(self, vgg_path, content,
+                style, content_weight,
+                style_weight, tv_weight,
+                initial=None):
+        self.vgg = vgg_network.VGG(vgg_path)
+        self.content = content
+        self.style = style
+        self.image = self._get_initial_image_or_random(initial)
+
+        loss_calculator = LossCalculator(self.vgg, self.image);
+
+        self.content_loss = loss_calculator.content_loss(content,
+                                                         self.CONTENT_LAYER,
+                                                         content_weight)
+
+        self.style_loss = loss_calculator.style_loss(style,
+                                                     self.STYLE_LAYERS,
+                                                     style_weight)
+
+        self.total_variation_loss = loss_calculator.tv_loss(self.image,
+                                                            self.content.shape,
+                                                            tv_weight)
+
+        self.loss = self.content_loss + self.style_loss + self.total_variation_loss
+
+
+    def _get_initial_image_or_random(self, initial):
+        if initial is None:
+            initial_image = tf.random_normal(self.content.shape)
+        else:
+            initial_image = self.vgg.preprocess(initial)
+        return tf.Variable(initial_image)
+
+    def _current_loss(self):
+        losses = {}
+        losses['content'] = self.content_loss.eval()
+        losses['style'] = self.style_loss.eval()
+        losses['total_variation'] = self.total_variation_loss.eval()
+        losses['total'] = self.loss.eval()
+        return losses
+
+    def train(self, learning_rate, iterations, checkpoint_iterations):
+
+        def is_last(i):
+            return i == iterations - 1
+
+        def is_checkpoint_iteration(i):
+            return (checkpoint_iterations and i % checkpoint_iterations == 0) or is_last(i)
+
+        def print_progress(i):
+            stdout.write('Iteration %d/%d\n' % (i + 1, iterations))
+
+        # optimizer setup
+        train_step = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
+
+        best_loss = float('inf')
+        best = None
+
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            for i in range(iterations):
+                print_progress(i)
+
+                train_step.run()
+
+                if is_checkpoint_iteration(i):
+                    current_loss = self.loss.eval()
+                    if current_loss < best_loss:
+                        best_loss = current_loss
+                        best = self.image.eval()
+                    yield (
+                        (None if is_last(i) else i),
+                        self.vgg.unprocess(best.reshape(self.content.shape[1:])),
+                        self._current_loss()
+                    )
+
+
+class LossCalculator:
+
+    def __init__(self, vgg, stylized_image):
+        self.vgg = vgg
+        self.network = vgg.net(stylized_image)
+
+    def content_loss(self, content, content_layer, content_weight):
+        # compute content features in feedforward mode
+        content_image = tf.placeholder('float', shape=content.shape)
+        content_net = self.vgg.net(content_image)
+
+        with tf.Session() as sess:
+            content_feature = content_net[content_layer].eval(
+                    feed_dict={content_image: self.vgg.preprocess(content)})
+
+
+        # content loss
+        content_loss = content_weight * (2 * tf.nn.l2_loss(
+                self.network[content_layer] - content_feature) /
+                content_feature.size)
+
+        return content_loss
+
+
+    def style_loss(self, style, style_layers, style_weight):
+        image = tf.placeholder('float', shape=style.shape)
+        style_net = self.vgg.net(image)
+
+        with tf.Session() as sess:
+            style_preprocessed = self.vgg.preprocess(style)
+
+            style_loss = 0
+
+            for layer in style_layers:
+                style_image_gram = self._calculate_style_gram_matrix_for(style_net,
+                                                                   image,
+                                                                   layer,
+                                                                   style_preprocessed)
+
+                input_image_gram = self._calculate_input_gram_matrix_for(layer)
+
+                style_loss += style_weight * (2 * tf.nn.l2_loss(input_image_gram - style_image_gram) / style_image_gram.size)
+
+        return style_loss
+
+    def tv_loss(self, image, shape, tv_weight):
+        # total variation denoising
+        tv_y_size = _tensor_size(image[:,1:,:,:])
+        tv_x_size = _tensor_size(image[:,:,1:,:])
+        tv_loss = tv_weight * 2 * (
+                (tf.nn.l2_loss(image[:,1:,:,:] - image[:,:shape[1]-1,:,:]) /
+                    tv_y_size) +
+                (tf.nn.l2_loss(image[:,:,1:,:] - image[:,:,:shape[2]-1,:]) /
+                    tv_x_size))
+
+        return tv_loss
+
+    def _calculate_style_gram_matrix_for(self, network, image, layer, style_image):
+        image_feature = network[layer].eval(feed_dict={image: style_image})
+        image_feature = np.reshape(image_feature, (-1, image_feature.shape[3]))
+        return np.matmul(image_feature.T, image_feature) / image_feature.size
+
+    def _calculate_input_gram_matrix_for(self, layer):
+        image_feature = self.network[layer]
+        _, height, width, number = map(lambda i: i.value, image_feature.get_shape())
+        size = height * width * number
+        image_feature = tf.reshape(image_feature, (-1, number))
+        return tf.matmul(tf.transpose(image_feature), image_feature) / size
+
+def _tensor_size(tensor):
+    from operator import mul
+    return reduce(mul, (d.value for d in tensor.get_shape()), 1)
